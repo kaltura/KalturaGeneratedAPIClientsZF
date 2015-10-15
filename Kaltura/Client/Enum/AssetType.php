0e1cd6facd8b6b348de3ce611f9a1068d575c09c
Kaltura CSI <community@kaltura.com>
2015-10-15 14:20:45 -0400

    New clientlib ver

--- Kaltura/Client/Enum/AssetType.php
+++ Kaltura/Client/Enum/AssetType.php
@@ -40,6 +40,7 @@ class Kaltura_Client_Enum_AssetType extends Kaltura_Client_EnumBase
 	const PDF = "document.PDF";
 	const SWF = "document.SWF";
 	const TIMED_THUMB_ASSET = "thumbCuePoint.timedThumb";
+	const TRANSCRIPT = "transcript.Transcript";
 	const FLAVOR = "1";
 	const THUMBNAIL = "2";
 	const LIVE = "3";
