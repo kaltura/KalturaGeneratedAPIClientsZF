436a56545699e088832e1a38bb05a62aefc00b3b
Kaltura CSI <community@kaltura.com>
2018-11-27 02:50:32 -0500

    New clientlib ver

--- Kaltura/Client/Enum/ReportType.php
+++ Kaltura/Client/Enum/ReportType.php
@@ -66,6 +66,8 @@ class Kaltura_Client_Enum_ReportType extends Kaltura_Client_EnumBase
 	const CITIES = "30";
 	const OPERATING_SYSTEM_FAMILIES = "32";
 	const BROWSERS_FAMILIES = "33";
+	const USER_ENGAGEMENT_TIMELINE = "34";
+	const UNIQUE_USERS_PLAY = "35";
 	const PARTNER_USAGE = "201";
 }
 
